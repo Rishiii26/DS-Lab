693dcb055f9c4a40ba2b99d75fd1e0762765d4d7
Rishabh Shandilya <161699606+Rishiii26@users.noreply.github.com>
2025-10-15 22:19:08 +0530

    Convert C++ program to C for missing number detection

--- missing.c
+++ missing.c
@@ -1,23 +1,25 @@
+#include <stdio.h>
 
-#include <bits/stdc++.h>
-
-using namespace std;
 int main()
 {
     int n;
-    cout << "Enter the size of array= ";
-    cin >> n;
+    printf("Enter the size of array = ");
+    scanf("%d", &n);
+
     int a[n];
     for (int i = 0; i < n; i++)
     {
-        cin >> a[i];
+        scanf("%d", &a[i]);
     }
+
     for (int i = 1; i <= n; i++)
     {
         if (a[i - 1] != i)
         {
-            cout << i << endl;
+            printf("%d\n", i);
             break;
         }
     }
+
+    return 0;
 }
